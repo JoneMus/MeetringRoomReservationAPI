f16cc3bc84d7396c0f954e82038691ae2c42e51e
Jonne M <jonne.mustajarvi@gmail.com>
2026-01-20 13:15:05 +0200

    Korjaus: Varausta ei voi tehdä huonenumeroon mikä on negatiivinen tai 0

--- Services/BookingService.cs
+++ Services/BookingService.cs
@@ -31,7 +31,11 @@ namespace MeetingRoomReservationAPI.Services
             if (booking.StartTime < DateTime.Now)
                 return (false, "Varausta ei voi tehdä menneisyyteen.", null);
 
-            // 3. Sääntö: Päällekkäisyyksien tarkistus
+            // 3. Sääntö: Varattavan huoneen numero ei voi olla negatiivinen tai 0
+            if (booking.MeetingRoomId <= 0)
+                return (false, "Huoneen numero ei voi olla negatiivinen tai 0", null);
+
+            // 4. Sääntö: Päällekkäisyyksien tarkistus
             bool isOverlapping = await _context.Bookings.AnyAsync(b =>
                 b.MeetingRoomId == booking.MeetingRoomId &&
                 booking.StartTime < b.EndTime &&
